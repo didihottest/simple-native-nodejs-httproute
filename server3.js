7549087c99f10edbd03c4eec60c35df676e395a8
didihottest <didihottest@gmail.com>
2021-03-12 18:21:47 +0700

    done

--- server3.js
+++ server3.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
+const fs = require("fs")
 app.use(express.static(__dirname));
 
-app.get("/", function(req, res) {
-   res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/game", function(req, res){
-    res.sendFile(__dirname + "/game.html");
+app.get("/cpuinfo", function (req, res) {
+    res.sendFile(__dirname + "/cpuinfo.html");
+    let currentCpuInfo = fs.readFile(__dirname + "/json/cpu.json", function (err, data) {
+        if (err) throw err;
+        let cpuinfo = data;
+        return cpuinfo;
+    });
+    console.log(currentCpuInfo);
+    // currentCpuInfo = JSON.parse(currentCpuInfo);
+    // // document.getElementById(".cpuinfo").innerHTML()
+    // console.log(currentCpuInfo);
+
 });
 
-app.listen(3000, function(){
+app.get("/meminfo", function (req, res) {
+    res.sendFile(__dirname + "/meminfo.html");
+});
+
+app.listen(3000, function () {
     console.log("server started at port 3000");
 });
